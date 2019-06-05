55c077a5dacb026e711a34a9d12a15d5bb0c0965
dan <dan@DESKTOP-HHK66MJ>
2019-06-05 14:09:04 +0600

    add some stylish changes to navigation menu

--- app.js
+++ app.js
@@ -7,16 +7,16 @@ let Campground = require("./models/campground");
 let Comment = require("./models/comment");
 let seedDb = require("./seeds");
 
-seedDb();
-
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname+"/public"));
 
+
+//RENDER LANDING PAGE
 app.get("/", (req, res)=>{
-    res.render("landing");
+    res.redirect("campgrounds");
 });
-
 
 //INDEX ROUT - show all campgrounds
 app.get("/campgrounds", (req, res)=>{
